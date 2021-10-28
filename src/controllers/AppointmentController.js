66d3ef6e54dede6871cda1468c64bf4873ce12fa
Rodrigo Sabino <rodrigosabinosdm@gmail.com>
2021-10-28 11:53:02 -0300

    Created appointment CRUD

--- src/controllers/AppointmentController.js
+++ src/controllers/AppointmentController.js
@@ -1,7 +1,51 @@
 const Appointment = require('../models/Appointment')
 
 class AppointmentController{
+    async search(req, res){
+        const {date, schedule} = req.body;
+
+        const appointment = await Appointment.find(date, schedule)
+
+        return res.status(200).json({appointment});
+
+    }
+    async create(req, res){
+        const { date, schedule, specialty, status } = req.body;
+
+        if(!date || !schedule || !specialty || !status){
+            return res.status(400).json({ error: "credential is missing" });
+        }
+
+        // const verification = await Patient.verify(name, email)
+
+        // if(verification.exists){
+        //     return res.status(400).json({ error: "user exists"})
+        // }
+
+        await Appointment.create(date, schedule, specialty, status);
+        return res.status(201).json({message: "appointment created successfully"})
+
+    }
+    async delete(req, res){
+        const {date, schedule} = req.body;
+
+        // verificar de existe uma conta com o email
+        const verifyAppointment = await Appointment.find(date, schedule);
+
+        if(verifyAppointment.length < 0){
+            return res.status(400).json({message: "Appointment not exists"})
+        }
+
+        await Appointment.delete(date, schedule);
+        return res.status(201).json({message: "Appointment deleted successfully"})
+
+
+
+    }
+    async edit(req, res){
+        
+    }
 
 }
 
-module.exports = new Appointment();
+module.exports = new AppointmentController();
--- src/models/Appointment.js
+++ src/models/Appointment.js
@@ -1,4 +1,46 @@
+const knex = require('../database')
+
 class Appointment{
+    async create(date, schedule, specialty, status) {
+        return await knex("appointments").insert({date, schedule, specialty, status})
+    }
+
+    async find(date, schedule){
+        const res = await knex("appointments").select("*").where({ date: date, schedule: schedule })
+        return res
+    }
+
+    // async verify(name, email){
+    //     const nameInUSe = await knex("appointments").select("*").where({ name: name});
+    //     if(nameInUSe.length > 0){
+    //         return {exists: true}
+    //     }
+    //     const emailInUSe = await knex("appointments").select("*").where({ email: email});
+    //     if(emailInUSe.length > 0){
+    //         return {exists: true}
+    //     }
+    //     return {exists: false}
+    // }
+
+    async delete(date, schedule){
+        try {
+            return await knex("appointments").select("*").where({ date: date, schedule: schedule }).del();
+        } catch (err) {
+            console.log("Patients.delete =>> ", err.message);
+        }
+    }
+
+    async update(){
+        try {
+
+    
+            return { success: true }
+        } catch (err) {
+            console.log("Patients.update =>> ", err.message);
+            return { success: false }
+        }
+
+    }
 
 }
 
