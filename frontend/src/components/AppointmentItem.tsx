7f9f2c5a13dcb610123b048ef2746625e084c4fd
Rodrigo Sabino <rodrigosabinosdm@gmail.com>
2021-10-28 21:14:38 -0300

    fix AppointmentItem

--- frontend/src/components/AppointmentItem.tsx
+++ frontend/src/components/AppointmentItem.tsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import deleteImg from '../assets/delete.png'
 
 export function AppointmentItem(){
+    const [status, setStatus] = useState("Pendente");
+    const [colorStatus, setColorStatus] = useState("yellow");
     function handleConfirmDelete(){
-        if(window.confirm('Are you sure you want to delete')){
+        if(!window.confirm('Are you sure you want to delete')){
             return
         }
+    }
+
+    function handleCheckedStatus(){
+
+        setStatus("Executado")
+        setColorStatus("green")
     }
     return(
         <div className="item-container">
             <p>Especialidade</p>
             <p>30/07/2001</p>
             <p>13:00h</p>
-            <p>status</p>
+            <button onClick={handleCheckedStatus}>
+                <p id={colorStatus}>{status}</p>
+            </button>
+
             <button onClick={handleConfirmDelete}>
                 <img  src={deleteImg} alt="lixeira" />
             </button>
