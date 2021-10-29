a1ca671d59c207d90301ce1e4f4115245a6ea2d5
Rodrigo Sabino <rodrigosabinosdm@gmail.com>
2021-10-29 13:22:17 -0300

    config integration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
 const {routes} = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
 
 app.use(express.json());
-app.use(routes)
+app.use(cors());
+app.use(routes);
 
 module.exports = { app };
