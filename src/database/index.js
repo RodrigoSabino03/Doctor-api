3ede4417898a58dd45cd8f3d8d868ebc8ad59556
RodrigoSabino03 <rodrigo.sabino@hygiasaude.com.br>
2023-06-13 14:09:35 -0300

    refactory

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const user = process.env.DATABASE_USER
+const database = process.env.DATABASE_NAME
+const password = process.env.DATABASE_PASSWORD
+const host = process.env.DATABASE_HOST
+const port = process.env.DATABASE_PORT
+
 const knex = require('knex')({
-    client: 'pg',
-    version: '7.2',
+    client: 'mysql2',
+    version: '5.7',
     connection: {
-      host : 'localhost',
-      port : 5432,
-      user : 'postgres',
-      password : '123',
-      database : 'hygia'
-    }
+      host : host,
+      port : port,
+      user: user,
+      database : database,
+      password : password,
+    },
+    database : database
   });
 
   module.exports = knex;
